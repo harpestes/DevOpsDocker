2e34770c7b13e281e5da4f501d7fa78d7b7afca9
harpestes <dikaloandrij@gmail.com>
2025-12-04 17:18:02 +0200

    fix: Fix init_db.sh script paths

--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export MSYS_NO_PATHCONV=1
+
 CONTAINER_NAME=${1:-devopsdocker-db-1}
 
 DB_USER="postgres"
@@ -8,9 +10,9 @@ DB_NAME="devOps"
 echo "Using container: $CONTAINER_NAME"
 
 echo "Copying SQL file to container..."
-docker cp ./sql/sh_insert.sql "$CONTAINER_NAME"://tmp/sh_insert.sql
+docker cp ./sql/sh_insert.sql "$CONTAINER_NAME":/tmp/sh_insert.sql
 
 echo "Applying SQL..."
-docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f //tmp/sh_insert.sql
+docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f /tmp/sh_insert.sql
 
 echo "Done"
